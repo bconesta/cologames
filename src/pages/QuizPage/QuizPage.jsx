e89776d34b10cb33bbcbaece4b882247fd83a617
TomasSerra <tomi.serra@gmail.com>
2023-02-13 15:39:29 -0300

    Estetica

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './QuizPage.scss'
 import Logo from '../../img/logos/logoAzul.png'
 
 export default function QuizPage() {
-  /*
+
+  const [question, setQuestion] = useState("");
+  const [option, setOption] = useState([]);
+  
   const preguntas = ["Pregunta 1", "Pregunta 2", "Pregunta 3", "Pregunta 4", "Pregunta 5"];
   const respuestasCorrectas = [1,0,2,1,3];
   const respuestas = [["Opcion A","Opcion B", "Opcion C", "Opcion D"],["Opcion A","Opcion B", "Opcion C", "Opcion D"],["Opcion A","Opcion B", "Opcion C", "Opcion D"],["Opcion A","Opcion B", "Opcion C", "Opcion D"],["Opcion A","Opcion B", "Opcion C", "Opcion D"]];
 
-  const questionText = document.getElementById("question");
-  const optionText = [document.getElementById("option1"),document.getElementById("option2"),document.getElementById("option3"),document.getElementById("option4") ]
+  useEffect(()=>{onLoad()}, [])
 
-  var totalQuestions = preguntas.length;
-  var questionNum = Math.floor(Math.random() * totalQuestions);
+  function onLoad(){
+    const questionText = document.getElementById("question");
 
-  questionText.innerHTML = preguntas[questionNum];
+    var totalQuestions = preguntas.length;
+    let questionNum = Math.floor(Math.random() * totalQuestions);
 
-  for(var i=0; i<=optionText.length; i++){
-    optionText[i].innerHTML = respuestas[questionNum][i];
+    var question = preguntas[questionNum];
+
+    for(var i=0; i<=option.length; i++){
+      optionText[i].innerHTML = respuestas[questionNum][i];
+    }
+
+    console.log(questionNum);
   }
-
-  console.log(questionNum);
-  */
+  
 
   function option(num){
-    /*
     if(num == respuestasCorrectas[questionNum]-1){
       console.log("Es correcta");
     }
     else{
       console.log("Es incorrecta");
     }
-    */
   }
 
 
@@ -42,21 +46,16 @@ export default function QuizPage() {
       <div id="questionNum">Pregunta 1/5</div>
       <center>
         <div className='question-container'>
-          <div id="question">Pregunta 1</div>
+          <div id="question">{question}</div>
         </div>
       
         <div className="options-container">
-          <button onClick={option(1)} id="option1">Opcion 1</button>
-          <button onClick={option(2)} id="option2">Opcion 2</button>
-          <button onClick={option(3)} id="option3">Opcion 3</button>
-          <button onClick={option(4)} id="option4">Opcion 4</button>
+          <button onClick={()=>{option(1)}} id="option1">{option[0]}</button>
+          <button onClick={()=>{option(2)}} id="option2">{option[1]}</button>
+          <button onClick={()=>{option(3)}} id="option3">{option[2]}</button>
+          <button onClick={()=>{option(4)}} id="option4">{option[3]}</button>
         </div>
       </center>
-
-
-
-
-
     </div>
   )
 }
