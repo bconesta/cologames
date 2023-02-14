f7000ee0f7af5a532a7b8bfa960b1b7d30779bd4
Bruno Conesta <bconesta@gmail.com>
2023-02-13 21:34:05 -0300

    Inital page setted

--- src/App.js
+++ src/App.js
@@ -23,25 +23,7 @@ function App() {
 
   const app = initializeApp(firebaseConfig)
 
-  function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
-
-  const [section, setSection] = useState(1);
+  const [section, setSection] = useState(0);
   const [theme, setTheme] = useState(0);
 
   const handleSection = (page) => {
