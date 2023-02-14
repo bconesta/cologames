b0aba46891aa835fb656e4329160859daf8b2b69
TomasSerra <tomi.serra@gmail.com>
2023-02-14 13:01:14 -0300

    Confetti god

--- src/pages/RoulettePage/RoulettePage.jsx
+++ src/pages/RoulettePage/RoulettePage.jsx
@@ -3,13 +3,15 @@ import Roulette from '../../components/Roulette'
 import './RoulettePage.scss'
 import Icono from '../../img/logos/icono.png'
 import Logo from '../../img/logos/logoAzul.png'
+import ReactConfetti from "react-confetti"
 
 export default function RoulettePage(props) {
     //Los elementos tienen que ser diferentes
     const elements = ["Auto", "Tecno", "Alquiler"]
 
     const [index, setIndex] = useState(0);
-    const [text, setText] = useState("¡Toca para jugar!")
+    const [text, setText] = useState("¡Toca para jugar!");
+    const [confetti, setConfetti] = useState(false);
 
     function handleIndex(i){
         setIndex(i)
@@ -20,12 +22,17 @@ export default function RoulettePage(props) {
 
     async function nextPage(){
         await setTimeout(()=>{
+            setConfetti(true)
+        }, 10000)
+        await setTimeout(()=>{
             props.handleSection(2)
+            setConfetti(false)
         }, 15000)
     }
 
     return (
         <div className='page'>
+            <ReactConfetti run={confetti} numberOfPieces={200} class="confetti" tweenDuration={10000}/>
             <div className="logo">
                 <img src={Logo} alt="nacion logo" />
             </div>
